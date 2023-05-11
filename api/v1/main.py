8197f7b42129f5e7ad4cdbcbaa9f0833dafd92a2
notwld <mwfarrukh@gmail.com>
2023-05-11 13:59:33 +0500

    fixed bugs

--- api/v1/main.py
+++ api/v1/main.py
@@ -1,30 +1,31 @@
 from fastapi import FastAPI, Request, Form, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 from Shortner.main import createTempObject
-from api.v1.db.firebase.db import saveUrlToFirestore, getUrlFromFirestore
+from .firebase.db import saveUrlToFirestore, getUrlFromFirestore
+from typing import Dict
 
 app = FastAPI()
 
 # Define the `root` route.
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 def root():
     return {"message": "Hello World"}
 
 
 # Define the `shorten` route.
 @app.post("/shorten")
-def shorten(request: Request, url: str = Form(...)):
+def shorten( url: str = Form(...)):
     # Create a temporary object.
     tempObj = createTempObject(url)
     # Save the short URL and long URL to Firestore.
     for shortUrl, longUrl in tempObj.items():
         saveUrlToFirestore(shortUrl, longUrl)
     # Return the index template with the short URL.
-    return {"request": request, "short_url": f"/{shortUrl}"}
+    return {"short_url": f"/{shortUrl}"}
 
 # Define the `redirectToUrl` route.
-@app.get("/{shortUrl}", response_class=RedirectResponse)
-def redirectToUrl(request: Request, shortUrl: str):
+@app.get("/{shortUrl}")
+def redirectToUrl(shortUrl: str):
     # Get the long URL from Firestore.
     longUrl = getUrlFromFirestore(shortUrl)
     # If the long URL is None, return an error message.
